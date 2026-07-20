6e5002972c3db4331e0ee64f63176818ea88572b
Alfat Taufik <alfattaufiknh@gmail.com>
2026-07-20 22:35:59 +0700

    phone detect

---
+++ homework/phone_number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int f_row, s_row, t_row;
+    printf("Enter phone number [(xxx) xxx-xxxx)]: ");
+    scanf("(%d) %d-%d", &f_row, &s_row, &t_row);
+    printf("You entered: %d.%d.%d", f_row, s_row, t_row);
+    return 0;
+}
