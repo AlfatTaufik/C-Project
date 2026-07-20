aabe5cfa489647cd4e21dceba2ec468f8c3a8102
Alfat Taufik <alfattaufiknh@gmail.com>
2026-07-20 22:56:37 +0700

    added new feature

--- homework/phone_number.c
+++ homework/phone_number.c
@@ -4,6 +4,6 @@ int main(void) {
     int f_row, s_row, t_row;
     printf("Enter phone number [(xxx) xxx-xxxx)]: ");
     scanf("(%d) %d-%d", &f_row, &s_row, &t_row);
-    printf("You entered: %d.%d.%d", f_row, s_row, t_row);
+    printf("You have entered: %d.%d.%d", f_row, s_row, t_row);
     return 0;
 }
