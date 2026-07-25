642b2af6a4c0b1d21b85ce252ad43f4b180f2612
Alfat Taufik <alfattaufiknh@gmail.com>
2026-07-25 15:31:25 +0700

    tambahan UPC program

--- homework/upccheckdigit.cpp
+++ homework/upccheckdigit.cpp
@@ -10,9 +10,9 @@ int main(){
 	&d1,&d2,&d3,&d4,&d5,&d6,&d7,&d8,&d9,&d10,&d11);
 	
 	first_sum = d1 + d3 + d5 + d7 + d9 + d11;
-	second_sum = d2, d4, d6, d8, d10;
+	second_sum = d2 + d4 + d6 + d8 + d10;
 	
-	total = 2 * first_sum + second_sum;
+	total = 3 * first_sum + second_sum;
 	
 	
 	printf("Check digit : %d\n", 9 - ((total - 1) % 10));
