832d768e245c23b47998fa00b3c32bb939fd7ef1
Alfat Taufik <alfattaufiknh@gmail.com>
2026-07-31 08:48:41 +0700

    random maps fix

--- homework/randmaps.c
+++ homework/randmaps.c
@@ -5,6 +5,8 @@
 int main()
 {
     char maps[10][10];
+    int row_now;
+    int cols_now;
     // char 'A', 'B', 'C';
 
     for (int i = 0; i < 10; i++)
@@ -16,16 +18,25 @@ int main()
     }
 
     srand(time(NULL));
+    row_now = rand() % 10;
+    cols_now = rand() % 10;
+    maps[row_now][cols_now] = 'A';
 
-    for (char letter = 'A'; letter <= 'Z'; letter++)
+    for (char letter = 'B'; letter <= 'Z'; letter++)
     {
-        int row_now;
-        int cols_now;
+        int move, next_row, next_cols;
         do
         {
-            row_now = rand() % 10;
-            cols_now = rand() % 10;
-        } while (maps[row_now][cols_now] != '.');
+            move = rand() % 4;
+            next_row = row_now;
+            next_cols = cols_now;
+            if(move == 0)next_cols--;
+            else if(move == 1)next_row++;
+            else if(move == 2)next_row--;
+            else if(move == 3)next_cols++;
+        } while (next_row < 0 || next_row >= 10 || next_cols < 0 || next_cols >= 10 || maps[next_row][next_cols] != '.' );
+        row_now = next_row;
+        cols_now = next_cols;
         maps[row_now][cols_now] = letter;
     }
 
